771907840acebe0ff930477aff3551a5afc2aaa1
19jackM <jack.mokoena1234@gmail.com>
2024-07-08 15:25:26 +0200

    added a stop decrement function

--- main.js
+++ main.js
@@ -7,30 +7,27 @@ function increment(){
 }
 
 function decrement(){
-
-isZero()
-counter--;
-console.log(counter);
+    
+    if (counter > 0){
+        
+        counter--;
+    console.log(counter);
+}
 }
 
-for (let counter = 0; counter < 0; counter--){
-    if (counter === 0) { break; }
-    text += "The number is " + counter + "<br>";
-  }
-    
-    if (counter <= 0 ){
+if (counter = 0){;
 
-        alert("decrement stopped")
-    }
+alert("decrement stopped")
+}
  
 
 
-function isZero(){
-    if (counter <= 0 ){
+// function isZero(){
+//     if (counter >= 0 ){
 
-        alert("decrement stopped")
-    }
-}
+//         alert("decrement stopped")
+//     }
+// }
 
 
 
