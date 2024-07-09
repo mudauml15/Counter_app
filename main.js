c75bf2700e11d8a3410d57019e73a864aab77b3a
19jackM <jack.mokoena1234@gmail.com>
2024-07-09 09:01:11 +0200

    added a display/read funtion for my app

--- main.js
+++ main.js
@@ -2,16 +2,23 @@ let counter = 0;
 
 function increment(){
     counter++;
-    console.log(counter);
+    // console.log(counter);
+let results = document.getElementById('display');
+    results.innerHTML= counter
+
 }
 
 function decrement(){
     if (counter > 0){
         counter--;
-        console.log(counter);
+        // console.log(counter);
+
+let results = document.getElementById('display');
+    results.innerHTML= counter
     } else {
         alert("Decrement stopped.");
     }
 }
+
 
 
